ee99c9d02e29eb5436ecaf7b37a93e3354150ed7
tomedwrds <tomedwards1056@outlook.com>
2023-01-11 13:20:58 +1300

    setup header file and implemented gpio driver

---
+++ Driver Development Project/Debug/drivers/Src/LedToggle.d
@@ -0,0 +1,7 @@
+drivers/Src/LedToggle.o: ../drivers/Src/LedToggle.c \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h:
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h:
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h:
---
+++ Driver Development Project/Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h \
+ C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h:
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx.h:
+C:/Users/tomed/Documents/GitHub/GPIO-Driver/Driver\ Development\ Project/drivers/Inc/stm32f407xx_gpio_driver.h:
